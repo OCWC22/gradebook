7176a10c5065382a1c9f3461b785d81070bae682
William Chen <57119977+OCWC22@users.noreply.github.com>
2023-01-11 09:07:19 -0800

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1};
+            var grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+            
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {   
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
